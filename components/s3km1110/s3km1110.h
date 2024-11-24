cd2775ffc4530315e9229da2cf1a7b9853d10637
Pawel Puterla <pawel.puterla@arelion.com>
2024-11-24 22:12:05 +0100

    test

--- components/s3km1110/s3km1110.h
+++ components/s3km1110/s3km1110.h
@@ -10,7 +10,7 @@ namespace s3km1110 {
 class S3KM1110Component : public Component, public uart::UARTDevice {
  public:
   S3KM1110Component() {
-      ESP_LOGD(TAG, "s3km1110 constructor!");
+      ESP_LOGD("ble", "s3km1110 constructor!");
   }
   void setup() override;
   void loop() override;
