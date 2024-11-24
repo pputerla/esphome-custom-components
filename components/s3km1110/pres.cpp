8893649f62ab54504a9260386f8af8be939f28ad
Pawel Puterla <pawel.puterla@arelion.com>
2024-11-24 16:36:06 +0100

    test

--- components/s3km1110/pres.cpp
+++ components/s3km1110/pres.cpp
@@ -5,7 +5,7 @@ namespace esphome {
 namespace s3km1110 {
 
 
-class S3KM1110Component : public Component {
+class S3KM1110Component : public Component, public uart::UARTDevice {
  public:
   void setup() override {
     // This will be called once to set up the component
