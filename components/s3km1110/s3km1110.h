2289145acc0facb35609912ac1517f46021a8c14
Pawel Puterla <pawel.puterla@arelion.com>
2024-11-24 22:28:51 +0100

    test

--- components/s3km1110/s3km1110.h
+++ components/s3km1110/s3km1110.h
@@ -2,12 +2,6 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
-#include "esphome/core/log.h"
-
-
-#ifdef USE_LOGGER
-#include "esphome/components/logger/logger.h"
-#endif
 
 namespace esphome {
 namespace s3km1110 {
@@ -15,9 +9,6 @@ namespace s3km1110 {
 
 class S3KM1110Component : public Component, public uart::UARTDevice {
  public:
-  S3KM1110Component() {
-      ESP_LOGD("ble", "s3km1110 constructor!");
-  }
   void setup() override;
   void loop() override;
 };
