9cedf3487cc4bd4b3dffb4d070080717c2961262
Pawel Puterla <pawel.puterla@arelion.com>
2024-11-24 16:45:49 +0100

    test

--- components/s3km1110/s3km1110.cpp
+++ components/s3km1110/s3km1110.cpp
@@ -1,19 +1,19 @@
 #include "esphome.h"
+#include "s3km1110.h"
 
 
 namespace esphome {
 namespace s3km1110 {
 
-class S3KM1110Component : public Component, public uart::UARTDevice {
- public:
-  void setup() override {
+
+  void S3KM1110Component::setup()  {
     // This will be called once to set up the component
     // think of it as the setup() call in Arduino
     ESP_LOGD("custom", "Component started");
 
 
   }
-  void loop() override {
+  void S3KM1110Component::loop()  {
     // This will be called very often after setup time.
     // think of it as the loop() call in Arduino
     /*if (digitalRead(5)) {
@@ -23,7 +23,6 @@ class S3KM1110Component : public Component, public uart::UARTDevice {
       ESP_LOGD("custom", "The GPIO pin 5 is HIGH!");
     }*/
   }
-};
 
 }
 }
--- components/s3km1110/s3km1110.h
+++ components/s3km1110/s3km1110.h
@@ -3,23 +3,8 @@
 
 class S3KM1110Component : public Component, public uart::UARTDevice {
  public:
-  void setup() override {
-    // This will be called once to set up the component
-    // think of it as the setup() call in Arduino
-    ESP_LOGD("custom", "Component started");
-
-
-  }
-  void loop() override {
-    // This will be called very often after setup time.
-    // think of it as the loop() call in Arduino
-    /*if (digitalRead(5)) {
-      digitalWrite(6, HIGH);
-
-      // You can also log messages
-      ESP_LOGD("custom", "The GPIO pin 5 is HIGH!");
-    }*/
-  }
+  void setup() override;
+  void loop() override;
 };
 
 }
